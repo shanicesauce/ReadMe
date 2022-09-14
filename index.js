b682417df51fff8bb3d955aeacfb45deb58335a7
shanicesauce <shanicesauceda@gmail.com>
2022-09-14 16:06:03 -0500

    generates readme

---
+++ index.js
@@ -0,0 +1,60 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./utils/generatePage')
+
+inquirer
+.prompt ([
+   {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+   },
+   { 
+      type: 'input' ,
+      name: 'description' ,
+      message: 'Provide a description of the project'
+   },
+   {
+    type: 'input' ,
+    name: 'installation' ,
+    message: 'How is your project installed?'
+    },
+    {
+    type: 'input' ,
+    name: 'usage' ,
+    message: 'How is your project used?'
+    },
+    {
+        type: 'input' ,
+        name: 'author' ,
+        message: 'List your collaborators'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+   } ,
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What did you build this project with? (check all that apply)',
+        choices: ['MIT', 'Apache', 'GNU', 'None']
+     } ,
+])
+.then((answers) =>{
+   console.log(answers);
+   fs.writeFile ('./README.md', generatePage(answers), (err) => {
+      if (err) {  
+       console.log(err)
+      } else {
+      console.log('the readme has been made')}
+    })
+})
+.catch (err => {
+   console.log(err);
+})
+
+
+
+
---
+++ utils/generatePage.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+function renderLicenseBadge(license) {
+    if (license === 'MIT'){
+    return `[![License](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+    } else if (license === 'Apache'){
+        return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]`
+    } else if (license === 'GNU'){
+        return `[![License](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+    } else {
+        return ''
+    }
+}
+function renderLicenseLink(license) {
+   if (license === 'MIT'){
+    return `(https://opensource.org/licenses/MIT)`
+   }  else if (license === 'Apache'){
+    return`(https://opensource.org/licenses/Apache-2.0)`
+    } else if (license === 'GNU'){
+        return `(https://www.gnu.org/licenses/gpl-3.0)`
+    } else {
+        return ''
+    }
+}
+function renderLicenseSection(license) {
+    return `${renderLicenseBadge(license)}${renderLicenseLink(license)}`
+}
+const generatePage = ({title , description, installation, usage,languages,author, license}) => {
+    return `# ${title}
+
+## Description
+    
+${description}
+    
+    
+## Table of Contents
+    
+If your README is long, add a table of contents to make it easy for users to find what they need.
+    
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${installation}
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+${usage}
+instructions for usage
+
+##Technologies Used
+${languages.join(' , ')}
+
+## Authors
+- ${author}
+
+${renderLicenseSection(license)}
+ `
+}
+
+  module.exports = generatePage
